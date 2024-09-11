05360972aa4887dfae50ebde5b6e8ddab6f27d38
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-11 17:46:21 -0400

    Set up an async function in index.js to hash a password

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const bcrypt = require('bcrypt');
+
+// Set up async function to hash a password
+const hashPassword = async(myPassword)=>{
+    const salt = await bcrypt.genSalt(12);
+    const hash = await bcrypt.hash(myPassword, salt);
+    console.log(`salt: ${salt}`);
+    console.log(`hash: ${hash}`);
+}
+
+hashPassword('hello1234');
