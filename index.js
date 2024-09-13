22fa59fbe4765d8fae124d41e14e359ea5b49ab8
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-12 23:09:21 -0400

    Set up a login function in index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const bcrypt = require('bcrypt');
 
-// Set up async function to hash a password
-const hashPassword = async(myPassword)=>{
-    const salt = await bcrypt.genSalt(12);
-    const hash = await bcrypt.hash(myPassword, salt);
-    console.log(`salt: ${salt}`);
-    console.log(`hash: ${hash}`);
-}
+// // Set up async function to hash a password
+// const hashPassword = async(myPassword)=>{
+//     const salt = await bcrypt.genSalt(12);
+//     const hash = await bcrypt.hash(myPassword, salt);
+//     console.log(`salt: ${salt}`);
+//     console.log(`hash: ${hash}`);
+// }
 
-hashPassword('hello1234');
+// hashPassword('hello1234');
+
+const login = async(pw, hashedPw) => {
+    const result = await bcrypt.compare(pw, hashedPw);
+
+    if (result) {
+        console.log("Logged you in! Successful match!")
+    } else {
+        console.log("Incorrect! Try again!")
+    }
+};
+
+login('hello1234', '$2b$12$i/evpc7QE51P42ZvSdJ9..HVqM.7JykoHGb2lVhy0tt8F.xvqrWOy');
